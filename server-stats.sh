9d0db54d46161b61da1c617acf6f15b4472d9211
BrettFord1999 <brettford1118@gmail.com>
2024-11-10 15:27:31 -0600

    Added Memory Usage

--- server-stats.sh
+++ server-stats.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
 # CPU Usage
-user_cpu=$(top -bn1 | grep "Cpu(s)" | awk -F ',' '{print $1}'| sed 's/%Cpu(s)://g')
-system_cpu=$(top -bn1 | grep "Cpu(s)" | awk -F ',' '{print $2}')
+user_cpu=$(top -bn1 | grep "Cpu(s)" | awk -F ',' '{print $1}'| sed 's/%Cpu(s)://g' | tr -dc '0-9.')
+system_cpu=$(top -bn1 | grep "Cpu(s)" | awk -F ',' '{print $2}' | tr -dc '0-9.')
+total_cpu=$(echo "$user_cpu + $system_cpu" | bc)
 
-echo "CPU Usage: $user_cpu $system_cpu"
+echo "CPU Usage: User $user_cpu System $system_cpu Total $total_cpu"
+
+# Memory Usage
+total_memory=$(free -m | grep "Mem:" | awk '{ print $2}')
+used_memory=$(free -m | grep "Mem:" | awk '{ print $3}')
+echo "Memory Usage: Total $total_memory Used $used_memory"
+#Calculate percentage of used memory
+used_memory_percentage=$(echo "scale=2; $used_memory / $total_memory * 100" | bc)
+echo "Memory Usage: Used $used_memory_percentage%"
