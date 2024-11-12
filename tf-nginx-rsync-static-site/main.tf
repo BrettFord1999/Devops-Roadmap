d22b9870f9632e807f5e76be7ec82f4a4b865138
BrettFord1999 <brettford1118@gmail.com>
2024-11-11 19:09:04 -0600

    update provisioner with testing

--- tf-nginx-rsync-static-site/main.tf
+++ tf-nginx-rsync-static-site/main.tf
@@ -14,9 +14,28 @@ resource "aws_instance" "web_server" {
       user        = "ubuntu"
       private_key = file("~/.ssh/key_pair")
       host        = self.public_ip
+      timeout     = "2m"
     }
+    
   provisioner "remote-exec" {
-    inline = ["echo 'Hello, World!'"]
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "timeout 30 sudo systemctl start nginx",
+      "timeout 30 sudo systemctl status nginx",
+      "echo 'Moving files...'",
+      "cd /var/www/html",
+      "mv dist/* . || echo 'Move failed'",
+      "echo 'Files moved successfully'",
+      "sudo systemctl stop nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl daemon-reload"
+    ]
+  }
+
+  timeouts {
+    create = "10m"
+    delete = "10m"
   }
 }
 
@@ -27,6 +46,8 @@ resource "aws_key_pair" "key_pair" {
 
 resource "aws_security_group" "web_server_sg" {
   name_prefix = "web_server_sg"
+  
+  # Ingress rules
   dynamic "ingress" {
     for_each = var.allowed_ports
     content {
@@ -36,9 +57,19 @@ resource "aws_security_group" "web_server_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+
+  # Egress rules
+  dynamic "egress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
 
 output "web_server_ip" {
   value = aws_instance.web_server.public_ip
 }
-
--- tf-nginx-rsync-static-site/setup.sh
+++ tf-nginx-rsync-static-site/setup.sh
@@ -1,3 +1,25 @@
 #I already have Terraform installed
 #I've already set environment variables for AWS credentials
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/key_pair
+ssh -i ~/.ssh/key_pair ubuntu@SERVERPUBLICIP
+
+<<COMMENT
+sudo apt update
+sudo apt install nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl status nginx
+
+cd /var/www/html
+
+sudo git clone https://github.com/StartBootstrap/startbootstrap-sb-admin.git
+sudo mv startbootstrap-sb-admin/* .
+sudo mv dist/* .
+sudo rm -rf startbootstrap-sb-admin
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+sudo systemctl stop nginx
+sudo systemctl start nginx
+sudo systemctl daemon-reload
+COMMENT
+
